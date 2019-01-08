04b9b1e79e6459d508a8dfb2e8b0b17d404c1144
ketan <44260070+ketandhoka@users.noreply.github.com>
2019-01-08 18:39:06 +0530

    folder added
    flks

---
+++ MySDK/MySDK.swift
@@ -0,0 +1,15 @@
+//
+//  MySDK.swift
+//  MySDK
+//
+//  Created by Ketan dhoka on 03/01/19.
+//  Copyright © 2019 Ketan dhoka. All rights reserved.
+//
+
+import Foundation
+
+public class MySDK {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
